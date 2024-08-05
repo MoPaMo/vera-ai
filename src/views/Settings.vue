0febdd549c2e65373498f03cbe34396bfbe1a4be
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-08-05 20:23:53 +0000

    Add more setting options

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <h1>Settings</h1>
-    <button @click="resetKeys">Reset Keys</button>
+    <button @click="resetKeys">Reset Keys &amp; Assitant</button>
+<br/>
+    <button @click="resetAssistant">Reset Assistant</button>
+
+
+    <br/><br/><br/>
+    <router-link to="/">
+      <button>Cancel</button>
+    </router-link>
   </div>
 </template>
 
@@ -10,6 +18,11 @@ export default {
   methods: {
     resetKeys() {
       localStorage.removeItem('apiKey');
+      localStorage.removeItem('agentId');
+      this.$router.push('/keys');
+    },
+    resetAssistant() {
+      
       localStorage.removeItem('agentId');
       this.$router.push('/setup');
     }
