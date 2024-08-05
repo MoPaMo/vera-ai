2d0c63b717390bba6e7ce8cb04165ef5d5c35dda
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-08-05 21:56:25 +0000

    Add checking

--- src/views/Key.vue
+++ src/views/Key.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="container">
     <h1>Enter API Key</h1>
@@ -6,6 +5,10 @@
       <input v-model="apiKey" placeholder="Enter your API key" />
       <button @click="saveApiKey">Save Key</button>
     </div>
+    <div>
+      <a href="https://platform.openai.com/api-keys" target="_blank">Get your API key here</a>
+    </div>
+    <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
   </div>
 </template>
 
@@ -51,19 +54,43 @@ button {
 button:hover {
   background-color: #369a6e;
 }
+
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.error {
+  color: red;
+  margin-top: 10px;
+}
 </style>
+
 <script>
 export default {
   data() {
     return {
       apiKey: '',
+      errorMessage: ''
     };
   },
   methods: {
     saveApiKey() {
-      localStorage.setItem('apiKey', this.apiKey);
-      this.$router.push('/setup');
+      if (this.isValidApiKey(this.apiKey)) {
+        localStorage.setItem('apiKey', this.apiKey);
+        this.$router.push('/setup');
+      } else {
+        this.errorMessage = 'Invalid API key format. Please enter a valid key.';
+      }
     },
-  },
+    isValidApiKey(key) {
+      const apiKeyPattern = /^sk-.+$/;
+      return apiKeyPattern.test(key);
+    }
+  }
 };
 </script>
