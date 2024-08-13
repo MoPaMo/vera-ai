98a4e97ae8fc9dae27c7cfd54fcb461e6854aabd
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-08-13 17:08:20 +0000

    Add icon component

---
+++ src/components/HCBIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hackclub-icon">
+    <img :src="iconUrl" :alt="glyph+ ' icon'" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HCBIcon',
+  props: {
+    color: {
+      type: String,
+      default:"black"
+    },
+    glyph: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    iconUrl() {
+      return `https://icons.hackclub.com/api/icons/${this.color}/${this.glyph}.svg`;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hackclub-icon {
+  display: inline-block;
+}
+.hackclub-icon img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
